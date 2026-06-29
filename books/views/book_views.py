346684056119cbf5a8b853cf1ec2c296c1844e5e
Shadow4400 <beherasusanta4400@gmail.com>
2026-06-29 14:44:50 +0530

    change book view

--- books/views/book_views.py
+++ books/views/book_views.py
@@ -58,3 +58,5 @@ class BookRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     def perform_destroy(self, instance):
         instance.is_deleted = True
         instance.save(update_fields=['is_deleted'])
+
+# changed some line in the view
